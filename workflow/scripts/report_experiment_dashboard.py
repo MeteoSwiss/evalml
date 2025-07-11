0ef76c4a3f4592c44bdd7b0ed4a4ab1e51dda300
Francesco Zanetta <zanetta.francesco@gmail.com>
2025-07-11 18:50:09 +0200

    add dashboard generation rule

---
+++ workflow/scripts/report_experiment_dashboard.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from itertools import product
+import random
+import argparse
+import logging
+
+import pandas as pd
+import jinja2
+
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+def read_verif_file(Path: str) -> pd.DataFrame:
+    """Read a verification file and return it as a DataFrame."""
+    df = pd.read_csv(
+        Path,
+        dtype={"lead_time": str},
+    )
+    df["lead_time"] = pd.to_timedelta(df["lead_time"])
+    df["lead_time"] = df["lead_time"].dt.total_seconds() / 3600  # convert to hours
+    return df
+
+
+def combine_verif_files(verif_files: Path, labels: list[str] | None = None) -> pd.DataFrame:
+    """
+    Combine multiple verification files into a single DataFrame.
+    Each file is expected to have a 'model' column indicating the model name.
+    """
+    df = pd.DataFrame()
+    for i, file in enumerate(verif_files):
+        _df = read_verif_file(file)
+        _df["model"] = labels[i] if labels is not None else file.parent.name
+        df = pd.concat([df, _df])
+    df = df.reset_index(drop=True)
+
+    return df
+
+
+def main(args):
+
+    # load and combine verification data
+    df = combine_verif_files(args.verif_files, args.labels)
+    df = df[(df["hour"] == "all") & (df["season"] == "all") & (df["init_hour"] == "all")]
+    print(df.head())
+
+    # get unique models and params
+    models = df["model"].unique()
+    params = df["param"].unique()
+
+    # get json string to embed in the HTML
+    df_json = df.to_json(orient="records", lines=False)
+    
+    # read script
+    with open(args.script, "r") as f:
+        js_src = f.read()
+
+    # generate HTML from Jinja2 template
+    environment = jinja2.Environment(loader = jinja2.FileSystemLoader(args.template.parent))
+    template = environment.get_template(args.template.name)
+    html = template.render(verif_data=df_json, js_src = js_src, models=models, params=params)
+
+    with open(args.output, "w") as f:
+        f.write(html)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate a dashboard for experiment verification data.")
+    parser.add_argument("--verif_files", type=Path, nargs="+",
+                        default=[],
+                        help="Paths to verification data files (not used in this mock).")
+    parser.add_argument("--labels", type=str, nargs="+",
+                        default=None,
+                        help="Labels for the verification data files. If not provided, labels will be derived from the file paths.")
+    parser.add_argument("--template", type=Path, required=True,
+                        help="Path to the Jinja2 template file.")
+    parser.add_argument("--script", type=Path, required=True,
+                        help="Path to the JavaScript source file for the dashboard.")
+    parser.add_argument("--output", type=Path, default=Path("dashboard.html"),
+                        help="Path to save the generated HTML dashboard file.")
+    args = parser.parse_args()
+
+    # check that number of labels matches number of files
+    if args.labels is not None and len(args.labels) != len(args.verif_files):
+        raise ValueError("Number of labels must match the number of verification files."
+                         f" Got {len(args.labels)} labels for {len(args.verif_files)} files.")
+    
+    main(args)
+
+"""
+Example usage:
+python report_experiment_dashboard.py \
+    --verif_files output/data/*/*/verif_aggregated.csv \
+    --template resources/report/dashboard/template.html.jinja2 \
+    --script resources/report/dashboard/script.js
+"""
