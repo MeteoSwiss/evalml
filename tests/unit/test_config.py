8dfb1d847754ee388e4a727a6d43d2dafc12773d
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2025-07-17 15:32:39 +0200

    Extend CLI and Snakefile for "showcase" command (#25)
    * refactor and extend cli.py
    
    * rename config
    
    * update tests
    
    * force specifying report name

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,15 +1,15 @@
 import pytest
 
-from evalml.config import ExperimentConfig
+from evalml.config import ConfigModel
 
 
 def test_example_config(example_config):
     """Test that the example config loads correctly."""
 
     # this shoudd not raise an error
-    _ = ExperimentConfig.model_validate(example_config)
+    _ = ConfigModel.model_validate(example_config)
 
     # this should raise an error
     del example_config["runs"]
     with pytest.raises(ValueError, match="Field required"):
-        _ = ExperimentConfig.model_validate(example_config)
+        _ = ConfigModel.model_validate(example_config)
