4fb8852eaa59855e3b8d9ed5d037f7d833b6597a
Jonas Bhend <jonas.bhend@meteoswiss.ch>
2025-10-28 13:48:36 +0100

    Add support for multiple regions

--- resources/report/dashboard/script.js
+++ resources/report/dashboard/script.js
@@ -14,7 +14,7 @@ const choicesInstances = {};
 
 choicesInstances["region-select"] = new Choices("#region-select", {
   searchEnabled: false,
-  removeItemButton: false, // single-select should not have remove button
+  removeItemButton: true,
   shouldSort: false,
   itemSelectText: "",
   placeholder: false
@@ -56,53 +56,68 @@ header = document.getElementById("header-text").textContent.trim()
 // Define base spec
 var spec = {
   "data": { "values": data },
-  "params": [
-    {
-      "name": "xZoom",
-      "select": {
-        "type": "interval",
-        "encodings": ["x"],
-        "zoom": "wheel![!event.shiftKey]"
-      },
-      "bind": "scales"
-    }
-  ],
-  "facet": {
-    "column": { "field": "param" },
-    "row": { "field": "metric" }
+  "config": {
+    "scale": { "continuousPadding": 1 }
   },
-  "spec": {
-    "mark": "line",
-    "width": 300,
-    "height": 200,
-    "encoding": {
-      "x": {
-        "field": "lead_time",
-        "type": "quantitative",
-        "axis": { "labels": true, "ticks": true },
-      },
-      "y": {
-        "field": "value",
-        "type": "quantitative",
-        "scale": { "zero": false }
-      },
-      "color": {
-        "field": "source",
-        "legend": { "orient": "top", "labelLimit": 1000, "symbolSize": 1000 }
-      }
-    },
-    "transform": [
-      {
-        "filter": { "param": "xZoom" }
-      }
-    ]
+  "facet": {
+    "row": { "field": "metric", "type": "nominal", "title": null },
+    "column": { "field": "param", "type": "nominal" , "title": null },
   },
   "resolve": {
     "scale": {
       "x": "shared",
       "y": "independent"
-    }
-  }
+    },
+  },
+  "spec": {
+    "params": [
+      {
+        "name": "xZoom",
+        "select": {
+          "type": "interval",
+          "encodings": ["x"],
+          "zoom": "wheel![!event.shiftKey]"
+        },
+        "bind": "scales"
+      }
+    ],
+    "mark": {"type": "line", "point": { "size": 50 } },
+    "width": 300,
+    "height": 200,
+    "encoding": {
+      "x": {
+        "field": "lead_time",
+        "type": "quantitative"
+      },
+      "y": {
+        "field": "value",
+        "type": "quantitative" ,
+          "scale": { "zero": false }
+      },
+      "color": {
+        "field": "source",
+        "type": "nominal",
+        "legend": { "orient": "top", "title": "Data Source", "offset": 0, "padding": 10 }
+      },
+      "shape": {
+        "field": "region",
+        "type": "nominal",
+        "legend": { "orient": "top", "title": "Region", "offset": 0, "padding": 10 }
+      },
+      "strokeDash": {
+        "field": "region",
+        "type": "nominal",
+      },
+      "tooltip": [
+        { "field": "region", "type": "nominal", "title": "Region" },
+        { "field": "source", "type": "nominal", "title": "Source" },
+        { "field": "param", "type": "nominal", "title": "Parameter" },
+        { "field": "metric", "type": "nominal", "title": "Metric" },
+        { "field": "lead_time", "type": "quantitative", "title": "Lead Time (h)" },
+        { "field": "value", "type": "quantitative", "title": "Value" }
+      ]
+    },
+  },
 };
 
 
@@ -113,9 +128,7 @@ function getSelectedValues(id) {
 }
 
 function updateChart() {
-  const rawRegions = getSelectedValues("region-select");
-  let selectedRegions = Array.isArray(rawRegions) ? rawRegions : (rawRegions ? [rawRegions] : []);
-
+  const selectedRegions = getSelectedValues("region-select");
   const selectedSources = getSelectedValues("source-select");
   const selectedparams = getSelectedValues("param-select");
   const selectedMetrics = getSelectedValues("metric-select");
@@ -123,12 +136,9 @@ function updateChart() {
   const newSpec = JSON.parse(JSON.stringify(spec));
   const filters = [];
 
+  newSpec.title = "Verification using " + header;
   if (selectedRegions.length > 0) {
     filters.push({ field: "region", oneOf: selectedRegions });
-    // Set the plot title to the selected region
-    newSpec.title = "Verification for region '" + selectedRegions[0] + "' using " + header;
-  } else {
-    newSpec.title = null;
   }
   if (selectedSources.length > 0) {
     filters.push({ field: "source", oneOf: selectedSources });
