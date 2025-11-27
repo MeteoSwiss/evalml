b8bf55a62bb2ffc16a27e0b1a4040a808a93f807
Francesco Zanetta <francesco.zanetta@meteoswiss.ch>
2025-11-27 11:34:51 +0100

    use forecast data samples

--- resources/inference/templates/icon-ch1_generate_templates.sh
+++ resources/inference/templates/icon-ch1_generate_templates.sh
@@ -2,17 +2,17 @@
 # Script to generate GRIB templates for ICON-CH1 model data
 
 # note: data at these paths might be remove in the future
-ANA_SAMPLE=/store_new/mch/msopr/osm/KENDA-CH1/ANA25/det/iaf2025010100
-FG_SAMPLE=/store_new/mch/msopr/osm/KENDA-CH1/FG25/det/lff2025010100
+SFC_SAMPLE=/store_new/mch/msopr/osm/ICON-CH1-EPS/FCST25/25010100_638/grib/i1eff00000000_000
+PL_SAMPLE=/store_new/mch/msopr/osm/ICON-CH1-EPS/FCST25/25010100_638/grib/i1eff00000000_000p
 
 # template for precipitation
-grib_copy -w shortName=TOT_PREC $FG_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-shortName=TOT_PREC.grib
+grib_copy -w shortName=TOT_PREC $SFC_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-shortName=TOT_PREC.grib
 
 # template for typeOfLevel=heightAboveGround
-grib_copy -w shortName=T_2M $ANA_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-typeOfLevel=heightAboveGround.grib
+grib_copy -w shortName=T_2M $SFC_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-typeOfLevel=heightAboveGround.grib
 
 # template for typeOfLevel=surface
-grib_copy -w shortName=PS $ANA_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-typeOfLevel=surface.grib
+grib_copy -w shortName=PS $SFC_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-typeOfLevel=surface.grib
 
 # template for typeOfLevel=isobaricInhPa
-grib_copy -w shortName=T,level=50 $ANA_SAMPLE /dev/stdout | grib_set -d 0 -s typeOfLevel=isobaricInhPa - icon-ch1-typeOfLevel=isobaricInhPa.grib
+grib_copy -w shortName=T,level=500 $PL_SAMPLE /dev/stdout | grib_set -d 0 - icon-ch1-typeOfLevel=isobaricInhPa.grib
