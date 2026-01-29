cbf0cb1ba9ec37cfb10167734e06aecac0af3df4
Daniele Nerini <daniele.nerini@meteoswiss.ch>
2026-01-29 14:35:33 +0100

    Fix cli, part 2

--- src/evalml/cli.py
+++ src/evalml/cli.py
@@ -72,19 +72,18 @@ def execute_workflow(
     command += config.profile.parsable()
     command += ["--configfile", str(configfile)]
     command += ["--cores", str(cores)]
-    command += ["--quiet", "rules"]  # reduce verobosity of snakemake output
 
     if dry_run:
         command.append("--dry-run")
     if unlock:
         command.append("--unlock")
-    if verbose:
-        command.append("--printshellcmds")
     if report and not dry_run:
         command += ["--report-after-run", "--report", str(report)]
 
     command.append(target)
     command += list(extra_smk_args)
+    if not verbose:
+        command += ["--quiet", "rules"]  # reduce verobosity of snakemake output
 
     raise SystemExit(run_command(command))
 
