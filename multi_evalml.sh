9c4cb47c82a1b0f7b3c013a50bfacf3edfd9208f
icedoom888 <alberto.pennino.8@gmail.com>
2026-02-09 11:11:01 +0100

    multi exp

---
+++ multi_evalml.sh
@@ -0,0 +1,5 @@
+evalml experiment experiments/10_vs_11_hidden.yaml --report
+evalml experiment experiments/graph_structures.yaml --report
+evalml experiment experiments/scratch_vs_finetune.yaml --report
+evalml experiment experiments/full_dataset_vs_cosmo_limited.yaml --report
+evalml showcase experiments/showcases.yaml
