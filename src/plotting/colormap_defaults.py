ff74be91730f2c4704b8593c451b7ad77be37c79
Michele Cattaneo <michelecattaneo18@gmail.com>
2026-07-03 15:23:34 +0200

    fix: fix bug for contorf plot for CLCT

--- src/plotting/colormap_defaults.py
+++ src/plotting/colormap_defaults.py
@@ -57,7 +57,7 @@ _CMAP_DEFAULTS = {
         "cmap": plt.get_cmap("Blues", 10),
         "vmin": 0,
         "vmax": 1,
-        "extend": "neither",
+        "extend": "both",
         "units": "",
         "levels": [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
     },
